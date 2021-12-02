add2d0a2a73cfde6ce91c966b57d31afd09504f8
Lucas Glimfjord <lucas@glimfjord.com>
2021-12-02 06:56:58 +0100

    Solves day01

---
+++ src/day01/README.md
@@ -0,0 +1,9 @@
+# 🎄 Advent of Code 2021 - day 1 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2021/day/1)
+
+## Notes
+
+Took longer than i thought, because of one simple error that took me longer realise than it should've. The test case provided ran fine and wasn't affected. I had forgot to parse the rawinput into ints for the comparison which threw of the count by 1 in part 1. And got very apparent trying to print the sum of the sliding windows in part 2. However figured it out eventually.
---
+++ src/day01/index.ts
@@ -0,0 +1,74 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((x) => parseInt(x));
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  var deeper: int = 0;
+
+  for (var i: int = 0; i < input.length; i++) {
+    if (input[i - 1] < input[i]) {
+      deeper += 1;
+    }
+  }
+
+  return deeper;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  var deeper: int = 0;
+
+  for (var i: int = 0; i < input.length - 3; i++) {
+    const slide1 = input[i] + input[i + 1] + input[i + 2];
+    const slide2 = input[i + 1] + input[i + 2] + input[i + 3];
+
+    if (slide1 < slide2) {
+      deeper++;
+    }
+  }
+
+  return deeper;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`,
+        expected: 7,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`,
+        expected: 5,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+});
